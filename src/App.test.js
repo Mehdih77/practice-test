14601908ed5af9a748b44e9a00930203c885a3b2
Mehdi <mehdi.havaei77@gmail.com>
2023-09-18 11:47:33 +0330

    add setup and teardown

--- src/App.test.js
+++ src/App.test.js
@@ -3,24 +3,47 @@ import App from "./App";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
-//? AAA pattern >>> Arrange , Act , Assert
-// Arrange:: preconditions for test
-// Act:: functions, clicks , renders ...
+// //? AAA pattern >>> Arrange , Act , Assert
+// // Arrange:: preconditions for test
+// // Act:: functions, clicks , renders ...
+
+// test("Testing React", () => {
+
+//   //! arrange
+//   const container = document.createElement("div");
+//   document.body.appendChild(container);
+
+//   //! act
+//   act(() => {
+//     render(<App />, container);
+//   });
+//   // render(<App />, container);
+
+//   //! assert
+//   expect(container.textContent).toBe("Hello");
+//   unmountComponentAtNode(container);
+//   container.remove();
+// });
+
+/* ---------------###############------------------------- */
+
+//! setup : execute before any test
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
 test("Testing React", () => {
-
-  //! arrange
-  const container = document.createElement("div");
-  document.body.appendChild(container);
-
-  //! act
   act(() => {
     render(<App />, container);
   });
-  // render(<App />, container);
-
-  //! assert
   expect(container.textContent).toBe("Hello");
+});
+
+//! teardown : execute after any test
+afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
+  container = null;
 });

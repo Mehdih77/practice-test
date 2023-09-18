ba80dcb216dddd6a9c79ce611d688894e14a7ae2
Mehdi <mehdi.havaei77@gmail.com>
2023-09-18 13:08:31 +0330

    test timing with jest

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount((prev) => prev + 1);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div>
       <p>{count}</p>
--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -6,28 +6,43 @@ let container = null;
 beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
+  jest.useFakeTimers();
+  //  jest.runAllTimers()
+  //  jest.clearAllTimers()
 });
 afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
+  jest.useRealTimers();
 });
 
 describe("counter", () => {
-  it("should be zero", () => {
+  //   it("should be zero", () => {
+  //     act(() => {
+  //       render(<Counter />, container);
+  //     });
+  //     const countElm = document.querySelector("p");
+  //     expect(countElm.textContent).toBe("0");
+  //   });
+  //   test("should be one", () => {
+  //     act(() => {
+  //       render(<Counter />, container);
+  //     });
+  //     const buttonElm = document.querySelector("button");
+  //     act(() => {
+  //       buttonElm.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  //     });
+  //     const countElm = document.querySelector("p");
+  //     expect(countElm.textContent).toBe("1");
+  //   });
+  //! in here we test the timing
+  test("should be one after 5second", () => {
     act(() => {
       render(<Counter />, container);
     });
-    const countElm = document.querySelector("p");
-    expect(countElm.textContent).toBe("0");
-  });
-  test("should be one", () => {
     act(() => {
-      render(<Counter />, container);
-    });
-    const buttonElm = document.querySelector("button");
-    act(() => {
-      buttonElm.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      jest.advanceTimersByTime(5000);
     });
     const countElm = document.querySelector("p");
     expect(countElm.textContent).toBe("1");

af3c1cb7f6142e6d6c2845ddd7bf5bc136017eea
Mehdi <mehdi.havaei77@gmail.com>
2023-09-18 11:34:45 +0330

    add act for test and use AAA pattern

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,26 @@
 // import { render, screen } from '@testing-library/react';
 import App from "./App";
 import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+//? AAA pattern >>> Arrange , Act , Assert
+// Arrange:: preconditions for test
+// Act:: functions, clicks , renders ...
 
 test("Testing React", () => {
+
+  //! arrange
   const container = document.createElement("div");
   document.body.appendChild(container);
 
-  render(<App />, container);
+  //! act
+  act(() => {
+    render(<App />, container);
+  });
+  // render(<App />, container);
 
+  //! assert
+  expect(container.textContent).toBe("Hello");
   unmountComponentAtNode(container);
   container.remove();
 });

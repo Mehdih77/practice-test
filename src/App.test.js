62766bf57ddad290288a4e63b31f84b5d8aa1c9e
Mehdi <mehdi.havaei77@gmail.com>
2023-09-18 14:03:40 +0330

    testing with testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,18 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("First Test", () => {
-  //   const { debug, container } = render(<App />);
+  //  const { debug, container } = render(<App />);
   render(<App />);
-  screen.debug();
+
+  expect(
+    screen.getByRole("heading", { name: /fruit form/i })
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("textbox", {
+      name: /first name/i,
+    })
+  ).toBeInTheDocument();
+
+  expect(screen.getAllByText(/fruit/i)).not.toBeNull();
 });

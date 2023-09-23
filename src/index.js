9de514d5a024769e5a75d9c4122947b090bba331
Mehdi <mehdi.havaei77@gmail.com>
2023-09-23 12:37:10 +0330

    test with userEvent

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Post from './Post';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Contact from "./Contact";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Post url="/post/2" />
+    <Contact />
   </React.StrictMode>
 );
 
